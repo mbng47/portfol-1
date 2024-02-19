3045caa1d82c049dcd654989948f0ddbffe3e103
Martin <martinbungner@hotmail.com>
2024-02-19 15:00:16 +0100

    header stuff

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,8 @@ export default function Header({ menuOpen, setMenuOpen }) {
     
 
     return (
-        <header className='w-screen fixed top-0 py-4 pr-8 pl-6 flex flex-row justify-between items-center border-2 border-black border-dotted'>
-            <h2 className="ml-9 text-2xl font-extrabold cursor-pointer">SWE/ENG</h2>
+        <header className='w-screen fixed top-0 pt-10 pb-2 pl-14 pr-8 bg-pink-BG flex flex-row justify-between items-center'>
+            <h2 className="text-2xl font-extrabold cursor-pointer">SWE/ENG</h2>
             <button onClick={() => { setMenuOpen(!menuOpen) }}
                 className="mr-12 flex flex-col justify-center items-center cursor-pointer">
                 <span className={`bg-white block transition-all duration-500 ease-out h-0.5 w-6 rounded-sm ${menuOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`} >
