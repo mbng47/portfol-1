490bab962cbd81be6d81f910777a598b4232b2ca
Martin <martinbungner@hotmail.com>
2024-04-03 21:41:10 +0200

    correcting header top position to page
    using useEffect to modify header's position prop when on top of page to not take space in document flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Menu from '../components/Menu';
 import Home from '../components/Home';
@@ -11,11 +11,12 @@ import Contact from '../components/Contact';
 export default function Page() {
 
   const [menuOpen, setMenuOpen] = useState(false);
+  const [stickyHeader, setStickyHeader] = useState(false);
 
   return (
     <>
       <main className={`z-10 min-h-screen w-screen absolute top-0 right-0 bg-pink-BG text-white duration-500 ${menuOpen ? '-translate-x-64' : ''}`} onClick={() => menuOpen ? setMenuOpen(false) : ''}>
-        <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} stickyHeader={stickyHeader} setStickyHeader={setStickyHeader}/>
         <Home />
         <About />
         <Projects />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,28 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+export default function Header({ menuOpen, setMenuOpen, stickyHeader, setStickyHeader }) {
 
-export default function Header({ menuOpen, setMenuOpen }) {
-    
+    useEffect(() => {
+        const handleScroll = () => {
+            setStickyHeader(window.scrollY > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <header className='w-screen sticky top-0 pt-12 pb-2 pl-16 pr-10 bg-pink-BG flex flex-row justify-between items-center border-b border-black bg-opacity-50'>
+        <header className={`w-screen ${stickyHeader ? 'sticky top-0' : 'absolute'} pt-12 pb-2 pl-16 pr-10 bg-pink-BG flex flex-row justify-between items-center border-b border-black bg-opacity-50`}>
             <h2 className="text-2xl font-extrabold cursor-pointer">SWE/ENG</h2>
-            <button onClick={() => { setMenuOpen(!menuOpen) }}
-                className="mr-12 flex flex-col justify-center items-center cursor-pointer">
-                <span className={`bg-white block transition-all duration-500 ease-out h-0.5 w-6 rounded-sm ${menuOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`} >
-                </span>
-                <span className={`bg-white block transition-all duration-500 ease-out h-0.5 w-6 rounded-sm my-0.5 ${menuOpen ? 'opacity-0' : 'opacity-100'}`} >
-                </span>
-                <span className={`bg-white block transition-all duration-500 ease-out h-0.5 w-6 rounded-sm ${menuOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`} >
-                </span>
+            <button onClick={() => { setMenuOpen(!menuOpen) }} className="mr-12 flex flex-col justify-center items-center cursor-pointer">
+                <span className={`bg-white block transition-all duration-500 ease-out h-0.5 w-6 rounded-sm ${menuOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`} ></span>
+                <span className={`bg-white block transition-all duration-500 ease-out h-0.5 w-6 rounded-sm my-0.5 ${menuOpen ? 'opacity-0' : 'opacity-100'}`} ></span>
+                <span className={`bg-white block transition-all duration-500 ease-out h-0.5 w-6 rounded-sm ${menuOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`} ></span>
             </button>
         </header>
-    )
+    );
 }
