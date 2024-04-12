1f3224f83c6de9cf514508d88c6593fb50091981
Martin <martinbungner@hotmail.com>
2024-04-12 21:34:21 +0200

    social icons, responsiveness

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,15 +7,13 @@ import profilePic from '../images/profilePic.png'
 export default function About() {
 
     return (
-        <>
-            <article id='about' className='h-screen w-full flex flex-col sm:flex-row justify-center items-center gap-9 px-10 sm:px-20 2xl:px-28 border-2 border-black border-dotted'>
-                <Image className='sm:max-w-xs sm:max-h-xs rounded-full' src={profilePic} alt='Profile picture of Martin'/>
-                <p className='text-lg'>
+            <article id='about' className='h-screen w-full pt-28 sm:pt-0 pb-10 flex flex-col sm:flex-row justify-center items-center gap-9 px-10 sm:px-20 2xl:px-28'>
+                <Image className='max-w-52 max-h-52 sm:max-w-72 sm:max-h-72 rounded-full' src={profilePic} alt='Profile picture of Martin'/>
+                <p className='text-lg overflow-y-scroll sm:overflow-y-visible'>
                     Better safe than sorry. That's my motto. Yeah, you do seem to have a little 'shit creek' action going. You know, FYI, you can buy a paddle. Did you not plan for this contingency? I mean the Starship Enterprise had a self-destruct button. I'm just saying. 
                     Look, let's start with some tough love, alright? Ready for this? Here it goes: you two suck at peddling meth. Period. Good! 'Oh boo-hoo, I won't cook meth anymore!' You're a crybaby! Who needs you?! Hey, I'm unplugging the website, so no more money laundering! How do you like that?! 
                     Great, perfect you know... this is just. I told her you were my A Team. Oh, hello Mrs. White, the good news is the IRS has been paid off, the bad news is... ach, Jesus!
                     </p>
             </article>
-        </>
     )
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,17 +1,22 @@
 'use client'
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
+import Icon from 'react-simple-icons';
 
 
 export default function Menu({ menuOpen, setMenuOpen }) {
 
     return (
-        <article className={`min-h-screen w-full flex flex-row justify-center`}>
-            <ul className={`text-3xl fixed inset-y-1/2 flex flex-col justify-center items-center gap-4 sm:gap-2 xl:gap-6`}>
+        <article className={`min-h-screen w-full flex flex-row justify-center border border-black border border-solid`}>
+            <ul className={`text-3xl fixed inset-y-1/2 flex flex-col justify-center items-center gap-4`}>
                 <li><Link to="home" smooth={true} duration={800} className="cursor-pointer" onClick={() => setMenuOpen(false)}>Home</Link></li>
                 <li><Link to="about" smooth={true} duration={800} className="cursor-pointer" onClick={() => menuOpen ? setMenuOpen(false) : ''}>About</Link></li>
                 <li><Link to="projects" smooth={true} duration={800} className="cursor-pointer" onClick={() => menuOpen ? setMenuOpen(false) : ''}>Projects</Link></li>
                 <li><Link to="contact" smooth={true} duration={800} className="cursor-pointer" onClick={() => menuOpen ? setMenuOpen(false) : ''}>Contact</Link></li>
+            </ul>
+            <ul>
+                <Icon name='github' size={30} style={{}} />
+                <Icon name='linkedin' size={30} style={{ color: '#0077b5' }} />
             </ul>
         </article>
     )
