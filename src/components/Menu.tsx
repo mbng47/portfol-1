599a4dbeeaa6a6e0f7f4e7c1800215507852c298
Martin <martinbungner@hotmail.com>
2024-04-13 16:47:02 +0200

    Update Menu.tsx

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,17 +7,19 @@ import Icon from 'react-simple-icons';
 export default function Menu({ menuOpen, setMenuOpen }) {
 
     return (
-        <article className={`min-h-screen w-full flex flex-row justify-center border border-black border border-solid`}>
-            <ul className={`text-3xl fixed inset-y-1/2 flex flex-col justify-center items-center gap-4`}>
+        <article className={`min-h-screen w-full flex flex-col justify-center items-center`}>
+            <section className='fixed'>
+            <ul className={`text-3xl flex flex-col items-center gap-4`}>
                 <li><Link to="home" smooth={true} duration={800} className="cursor-pointer" onClick={() => setMenuOpen(false)}>Home</Link></li>
                 <li><Link to="about" smooth={true} duration={800} className="cursor-pointer" onClick={() => menuOpen ? setMenuOpen(false) : ''}>About</Link></li>
                 <li><Link to="projects" smooth={true} duration={800} className="cursor-pointer" onClick={() => menuOpen ? setMenuOpen(false) : ''}>Projects</Link></li>
                 <li><Link to="contact" smooth={true} duration={800} className="cursor-pointer" onClick={() => menuOpen ? setMenuOpen(false) : ''}>Contact</Link></li>
             </ul>
-            <ul>
+            <ul className={'flex justify-center pt-5 gap-3'}>
                 <Icon name='github' size={30} style={{}} />
                 <Icon name='linkedin' size={30} style={{ color: '#0077b5' }} />
             </ul>
+            </section>
         </article>
     )
 }
