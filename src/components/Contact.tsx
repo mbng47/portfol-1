e061a4fc51bd3f52e91b04f07f49e86ee3a2f46a
Martin <102648195+mbng47@users.noreply.github.com>
2024-07-10 00:16:36 +0200

    contact form
    fixed emailjs and added confirmation pop up toaster

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,15 @@
 'use client'
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { ToastContainer, toast, Flip } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
-const serviceId = process.env.EMAILJS_SERVICE_ID;
-const templateId = process.env.EMAILJS_TEMPLATE_ID;
-const publicKey = process.env.EMAILJS_PUBLIC_KEY;
+const serviceId = 'contact_service';
+const templateId = "contact_form";
+const publicKey = "xmS6JMCyCIQ3SY39O";
 console.log(serviceId, templateId, publicKey);
+
 
 
 // ---------------------------------------------------
@@ -21,60 +24,60 @@ export default function Contact() {
     emailjs
       .sendForm(
         serviceId ?? "",
-        process.env.EMAILJS_TEMPLATE_ID ?? "",
+        templateId ?? "",
         e.target,
-        process.env.EMAILJS_PUBLIC_KEY
+        publicKey ?? ""
       )
       .then(
         (result) => {
-          setStateMessage('Message sent!');
+          toast.success('Your email has been delivered', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          // setStateMessage('Message sent');
           setIsSubmitting(false);
-          setTimeout(() => {
-            setStateMessage(null);
-          }, 5000); // hide message after 5 seconds
+          // setTimeout(() => {
+          //   setStateMessage(null);
+          // }, 5000); // hide message after 5 seconds
         },
         (error) => {
-          setStateMessage('Something went wrong, please try again later');
+          toast.error('Something went wrong, please try again later', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+          // setStateMessage('Something went wrong, please try again later');
           setIsSubmitting(false);
-          setTimeout(() => {
-            setStateMessage(null);
-          }, 5000); // hide message after 5 seconds
+          // setTimeout(() => {
+          //   setStateMessage(null);
+          // }, 5000); // hide message after 5 seconds
         }
       );
 
-    // Clears the form after sending the email
+    // Clears form after sending email
     e.target.reset();
   };
 
   return (
-    <article id='contact' className='h-screen w-full pt-52 md:max-xl:pt-40 px-7 sm:px-20 flex flex-col border-2 border-black border-dotted'>
+    <article id='contact' className='h-screen w-full pt-20 md:pt-36 2xl:pt-52 px-7 sm:px-20 flex flex-col'>
                   <h2 className='uppercase font-[999] text-5xl sm:text-7xl'>Contact me</h2>
-                  {/* <h3 className='uppercase font-[900] text-5xl sm:text-6xl'>I always answer</h3> */}
-                  <p className='text-xl font-normal mt-5 max-w-700px'>Welcome to my site. I'm a software developer from Sweden residing in Malmö.</p>
+                  <p className='text-xl font-normal mt-5 max-w-700px'>You can send me an email via this form.</p>
       <form className={'pt-4 flex flex-col gap-2'} onSubmit={sendEmail}>
         <input className={'p-1 bg-white text-black font-medium rounded sm:max-w-2xl'} type="text" name="user_name" title='user_name' placeholder='Name' />
         <input className={'p-1 bg-white text-black font-medium rounded sm:max-w-2xl'} type="email" name="user_email" title='user_email' placeholder='Email' />
         <textarea className={'p-1 bg-white text-black font-medium rounded max-w-3xl basis-52'} name="message" title='message' placeholder='Message' />
         <input className={'bg-white text-black max-w-14 cursor-pointer rounded border border-black focus:outline focus:outline-sky-400'} type="submit" value="Send" disabled={isSubmitting} />
-        {stateMessage && console.log(stateMessage)}
       </form>
+      <ToastContainer transition={Flip}/>
     </article>
   );
 };
-
-
-
-
-// ----------------------------------------------------------
-
-// export default function Contact() {
-
-
-//     return (
-//             <article id='contact' className='h-screen w-full border-2 border-black border-dotted'>
-//             <form action="">
-//                 <input type="text" title='fname' placeholder='Chimp Apansson' />
-//             </form>
-//             </article>
-//     )
-// }
